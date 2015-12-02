b9fae48decd20073d812ed9f7886276702f68ae7
Marc Phillips <mphillips@outlook.com>
2015-12-02 16:18:01 +0000

    Updated the installed apps

--- pianoly/settings.py
+++ pianoly/settings.py
@@ -36,6 +36,7 @@ LOGIN_REDIRECT_URL = reverse_lazy("homepage")
 # Application definition
 
 INSTALLED_APPS = (
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
